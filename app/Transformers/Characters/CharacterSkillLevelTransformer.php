859576b99ee4a8355a64e643ad0c1b507337d8d5
Blede <bledegraphics@gmail.com>
2024-11-16 11:28:34 -0500

    Updated submodule public/ArknightsGameData

---
+++ app/Transformers/Characters/CharacterSkillLevelTransformer.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace App\Transformers\Characters;
+
+class CharacterSkillLevelTransformer {}
---
+++ app/Transformers/Characters/CharacterSkillLevelUpCostTransformer.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Transformers\Characters;
+
+class CharacterSkillLevelUpCostTransformer
+{
+
+}
